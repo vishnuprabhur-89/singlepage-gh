de2528e865eb2e9b4332437715d9a919a1b1999e
vishnuprabhur89 <vishnu.prabhu@mygate.in>
2021-06-21 16:24:37 +0530

    target timer updated

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
+import Plan from './plan.jpg';
+import React, { useEffect } from 'react';
 import './App.css';
 
-function App() {
+const SinglePage = () => {
+  const deadline = '2021-10-31';
+  const [dateObj, setDateObj] = React.useState({});
+  useEffect(() => {
+    const timeinterval = setInterval(() => {
+      setDateObj(updateTimer())
+      if (updateTimer().total <= 0) {
+        clearInterval(timeinterval);
+      }
+    }, 1000);
+
+  })
+  const updateTimer = () => {
+    const total = Date.parse(deadline) - Date.parse(new Date());
+    const seconds = Math.floor((total / 1000) % 60);
+    const minutes = Math.floor((total / 1000 / 60) % 60);
+    const hours = Math.floor((total / (1000 * 60 * 60)) % 24);
+    const days = Math.floor(total / (1000 * 60 * 60 * 24));
+    return { days, hours, minutes, seconds, total };
+  }
   return (
-    <div className="App">
+    <div className="App" id="time">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>Journey starts...</p>
+        <div style={{display: "flex"}}>
+          <h3>{`Days ${dateObj?.days} `}</h3>
+          <h3>{`Hours ${dateObj?.hours} `}</h3>
+          <h3>{`Minutes ${dateObj?.minutes} `}</h3>
+          <h3>{`Seconds ${dateObj?.seconds} `}</h3>
+        </div>
+        <img src={Plan} className="App-logo" alt="logo" />
       </header>
     </div>
   );
 }
 
-export default App;
+export default SinglePage;
